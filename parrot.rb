309d831c746e79e3af316cca682775686691f51f
Maurice Falls <falls8019oct@gmail.com>
2020-03-14 23:07:44 +0000

    Automatically backed up by Learn

--- parrot.rb
+++ parrot.rb
@@ -1,2 +1,12 @@
 # Create method `parrot` that outputs a given phrase and
 # returns the phrase
+
+def parrot
+  puts "Squawk!"
+  "Squawk!"
+ end
+ def parrot(name)
+   puts "Pretty bird!"
+   "Pretty bird!"
+ end
+ end
